8804b46db0c50ae4cde8c529212ff9bfce78b6c3
ayou6 <ayoxb.abbas@gmail.com>
2023-05-18 16:03:09 +0400

    user input

--- src/main/java/org/example/MeasurementSolver.java
+++ src/main/java/org/example/MeasurementSolver.java
@@ -7,6 +7,7 @@ public class MeasurementSolver {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter the equation: ");
+        String equation = scn.nextLine();
 
 
 
